9f58efef5ba754be183071223fa85edc6528ae3b
Derek <dereklau15@gmail.com>
2023-08-27 01:07:21 +0900

    Work on changing display info

--- src/interface.js
+++ src/interface.js
@@ -2,20 +2,47 @@ import { API_KEY, getCityData } from "./weather.js";
 
 const location = document.getElementById("location");
 const search = document.getElementById("search");
+const city = document.getElementById("city");
+const date = document.getElementById("date");
+const countryName = document.getElementById("country");
+const error = document.querySelector(".error");
 
 function searchCity() {
   search.addEventListener("click", function () {
-    let cityName = location.value.trim();
+    const cityName = location.value.trim();
     if (!cityName) return;
-    console.log(cityName);
-
     const GEOCODING_API = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`;
     getCityData(GEOCODING_API);
   });
+}
+
+function changeInfo(name, country) {
+  city.textContent = name;
+  countryName.textContent = country;
+}
+
+function changeDate(dateData) {
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  };
+  date.textContent = new Date(dateData).toLocaleDateString("en-GB", options);
+}
+
+function displayError() {
+  location.style.marginTop = "2.1rem";
+  error.style.display = "block";
+}
+
+function removeError() {
+  location.style.marginTop = "0rem";
+  error.style.display = "none";
 }
 
 function loadFunctions() {
   searchCity();
 }
 
-export { loadFunctions };
+export { loadFunctions, changeInfo, displayError, removeError, changeDate };
--- src/weather.js
+++ src/weather.js
@@ -1,15 +1,42 @@
+import {
+  changeInfo,
+  displayError,
+  removeError,
+  changeDate,
+} from "./interface.js";
+
 const API_KEY = "226dbf12f6c9f3e021556ea66e7c95c9";
 
 async function getCityData(API) {
   const response = await fetch(API, { mode: "cors" });
   const cityData = await response.json();
   if (!cityData.length) {
-    const error = document.querySelector(".error");
-    const location = document.getElementById("location");
-    location.style.marginTop = "2.1rem";
-    error.style.display = "block";
+    displayError();
+  } else {
+    const { name, country, lat, lon } = cityData[0];
+    changeInfo(name, country);
+    removeError();
+    getWeatherData(lat, lon, API_KEY);
   }
-  console.log(cityData);
+}
+
+async function getWeatherData(latency, longitude, API) {
+  const FORECAST_API = `http://api.openweathermap.org/data/2.5/forecast?lat=${latency}&lon=${longitude}&appid=${API}&units=metric`;
+  const response = await fetch(FORECAST_API, { mode: "cors" });
+  const weatherData = await response.json();
+  filterForecastData(weatherData);
+}
+
+function filterForecastData(data) {
+  changeDate(data.list[0].dt_txt);
+  console.log(data);
+  let uniqueForecastDays = [];
+  const sevenForecastDays = data.list.filter((forecast) => {
+    const forecastDate = new Date(forecast.dt_txt).getDate();
+    if (!uniqueForecastDays.includes(forecastDate)) {
+      return uniqueForecastDays.push(forecastDate);
+    }
+  });
 }
 
 export { getCityData, API_KEY };
